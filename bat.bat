9438ed4d4fcf9fa7230a4e4535e3ddfe700eeef3
BrecadoDev <gging3709@gmail.com>
2025-04-17 10:21:13 -0300

    Add files via upload

---
+++ bat.bat
@@ -0,0 +1,7 @@
+echo "# trigger" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BrecadoDev/TriggerSt.git
+git push -u origin main
